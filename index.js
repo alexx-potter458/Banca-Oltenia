d507a50ab96f3de362acd9578b2c425922b00a42
alexx-potter458 <alexx.potter458@gmail.com>
2021-05-01 19:56:12 +0300

    cerintele 1-2-3

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const createServerExpress = require('express');
-const myServer = createServerExpress(); // server as object
+const server = createServerExpress(); // server as object
+const path = require("path");
+
+server.set("view engine", "ejs");
+
+server.use("/src", createServerExpress.static(path.join(__dirname, "src")));
+
+server.get("/", function (req, res) {
+
+    res.render("pages/index.ejs");
+
+});
+
+server.get("/empty", function(req,res){
+    res.render("empty.ejs");
+})
 
 
+// un exemplu
+server.get("/data", function (req, res) {
+    res.setHeader("Content-type", "text/html");
+    console.log("salut3");
+    res.write("<!DOCTYPE html><html><body>" + new Date());
+    res.write("</body></html>");
+    res.end();
+});
 
-app.listen(8080);
+
+server.listen(8080);
 console.log("Am pornit serverul");
