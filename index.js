633ed9a7e07f3912f20536fc4d5462165e0014f3
alexx-potter458 <alexx.potter458@gmail.com>
2021-06-05 13:00:00 +0300

    urc

--- index.js
+++ index.js
@@ -60,14 +60,23 @@ function pictureCheck() {
 
 }
 
+// server.get("/", function(req, res) {
+
+//     var userIp = requestIp.getClientIp(req);
+//     let galleryPaths = pictureCheck();
+//     const result = client.query("select * from products where special = true", function(err, queryResult) {
+//         res.render("pages/index", { userIp: userIp, images: galleryPaths, serverIp: serverIp, products: queryResult.rows });
+//     });
+// });
+
 server.get("/", function(req, res) {
 
     var userIp = requestIp.getClientIp(req);
     let galleryPaths = pictureCheck();
-    const result = client.query("select * from products where special = true", function(err, queryResult) {
-        res.render("pages/index", { userIp: userIp, images: galleryPaths, serverIp: serverIp, products: queryResult.rows });
-    });
+    res.render("pages/index", { userIp: userIp, images: galleryPaths, serverIp: serverIp });
+
 });
+
 
 server.get("/index", function(req, res) {
 
