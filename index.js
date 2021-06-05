fe61e0bf54bcb2e0f505989ea8af204482e19284
alexx-potter458 <alexx.potter458@gmail.com>
2021-06-05 14:57:27 +0300

    database working

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ var herokuPort = process.env.PORT || 8080
 
 const client = new Client({
     host: 'ec2-54-225-228-142.compute-1.amazonaws.com', user: 'upopbwnaehhuib',
-    password: 'bcc08249637396c43f1b55e63fbe68b8cadfa7e8ce80d7210cb18b45b6ceb37e', database: 'dfa2i2emu7diij', port: 5432, ssl: true
+    password: 'bcc08249637396c43f1b55e63fbe68b8cadfa7e8ce80d7210cb18b45b6ceb37e', database: 'dfa2i2emu7diij', port: 5432, 
+    ssl: {require: true, rejectUnauthorized: false}
 });
+
 client.connect();
 
 var serverIp = serverNetwork.address();
